948b17fa941f8f486cb6530eabac25feb7135713
Matt Hughes <mhughes.ipod@gmail.com>
2022-12-20 20:02:10 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import { TwitterApi } from 'twitter-api-v2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const handleTweet = () => {
+    const twitterClient = new TwitterApi({
+        appKey: process.env.CONSUMER_KEY ?? '',
+        appSecret: process.env.CONSUMER_SECRET ?? '',
+        accessToken: process.env.ACCESS_TOKEN ?? '',
+        accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
+    });
+
+    const tweetClient = twitterClient.readWrite;
+
+    tweetClient.v2.tweet('very nice');
+};
+
+handleTweet();
