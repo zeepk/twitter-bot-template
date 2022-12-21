5db4dc4ffafbde447cb2b0b359c00e44d3623ac5
Matt Hughes <mhughes.ipod@gmail.com>
2022-12-20 20:14:34 -0800

    time

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const handleTweet = () => {
 
     const tweetClient = twitterClient.readWrite;
 
-    tweetClient.v2.tweet('very nice');
+    tweetClient.v2.tweet('Milliseconds since 01/01/1970: ' + Date.now());
 };
 
 handleTweet();
